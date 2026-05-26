d2ad63798eb6ba89daf49e58cdad66c02eb0edaa
Javier J. Martínez M. <jj_dev@outlook.com>
2026-05-25 22:32:17 -0600

    feat: add a BMI calculator for exercise 3.1
    + Add BMI calculator function.
    + Add scripts for typechecking and running the program.

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,44 @@
+interface BodyValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BodyValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (h: number, w: number) => {
+  // converting cm to m
+  const cmH = h / 100;
+  const bmi = w / (cmH * cmH);
+
+  if (bmi < 16) return "Underweight (Severe thinness)";
+  else if (bmi >= 16 && bmi < 17.0) return "Underweight (Moderate thinness)";
+  else if (bmi >= 17 && bmi < 18.5) return "Underweight (Mild thinness)";
+  else if (bmi >= 18.5 && bmi < 25) return "Normal range";
+  else if (bmi >= 25 && bmi < 30) return "Overweight (Pre-obese)";
+  else if (bmi >= 30 && bmi < 35) return "Obese (Class I)";
+  else if (bmi >= 35 && bmi < 40) return "Obese (Class II)";
+  else if (bmi >= 40) return "Obese (Class III)";
+  else throw new Error(`Can't calculate your BMI!`);
+};
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}

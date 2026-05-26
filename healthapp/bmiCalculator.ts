3faa61c0645df99af48f1f6b1970287a653a2550
Javier J. Martínez M. <jj_dev@outlook.com>
2026-05-26 14:25:04 -0600

    chore: clean unused function in bmiCalculator

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,22 +1,3 @@
-interface BodyValues {
-  height: number;
-  weight: number;
-}
-
-const parseArguments = (args: string[]): BodyValues => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error("Provided values were not numbers!");
-  }
-};
-
 const calculateBmi = (h: number, w: number) => {
   // converting cm to m
   const cmH = h / 100;

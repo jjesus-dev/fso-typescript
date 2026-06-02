beb98c2ca1484b4ce4fa32a33debc4e0a3d1406b
Javier J. Martínez M. <jj_dev@outlook.com>
2026-06-01 20:52:58 -0600

    feat: add routes, services & dummy data for diaries

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import diaryRouter from "./routes/diaries.ts";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.json({ message: "pong!" });
 });
+
+app.use("/api/diaries", diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ patientor/backend/src/services/diaryService.ts
@@ -0,0 +1,11 @@
+import diaryData from "../../data/entries.json" with { type: "json" };
+
+const getEntries = () => {
+  return diaryData;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default { getEntries, addDiary };
